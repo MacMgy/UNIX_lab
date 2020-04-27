0ce99c1a97121c7ef49fb32fe23a505a7bea0581
a.osipov <a.osipov@bi.zone>
2020-04-27 18:13:21 +0700

    Refactor clean up

--- Lab1/builder.sh
+++ Lab1/builder.sh
@@ -2,16 +2,10 @@
 
 NORMALTEXT="\033[0m"
 
-function handle_SIGNALS() {
-  rm -rf -- $mktemp_name
-  error "User kill process."
-}
-
 function succeful_message() {
     GREEN='\033[0;32m'
     echo ""
     printf "${GREEN} *** %s ${NORMALTEXT}\n" $1
-    echo ""
 }
 
 function error() {
@@ -22,16 +16,26 @@ function error() {
   exit 1
 }
 
+function clean_up() {
+  rm -rf -- $mktemp_name
+  [ ! -z $1 ] && error $1
+}
+
+function handle_SIGNALS() {
+  clean_up
+  error "User kill process."
+}
+
 filename="$1"
 
 [ -z "$filename" ] && error 'First argument must be a file name'
-[ ! -e "$filename" ] && error 'File does not exist'
-[ ! -r "$filename" ] && error 'File can not be read'
+[ ! -e "$filename" ] && error 'There is no file, you are looking for'
+[ ! -r "$filename" ] && error 'Faild to get permission, when try read file'
 
 OUTPUT_REGEX="s/^[[:space:]]*\/\/[[:space:]]*Output[[:space:]]*\([^ ]*\)$/\1/p"
 
 executable_file_name=$(sed -n -e "$OUTPUT_REGEX" "$filename" | grep -m 1 "")
-[ -z "$executable_file_name" ] && error 'Output name is not found'
+[ -z "$executable_file_name" ] && error 'Output name is not found or empty'
 
 echo "Create temporary folder..."
 mktemp_name=$(mktemp -d -t temp) || error 'Failed to create temp folder'
@@ -39,15 +43,15 @@ mktemp_name=$(mktemp -d -t temp) || error 'Failed to create temp folder'
 trap handle_SIGNALS HUP INT QUIT PIPE TERM
 
 echo "Copying src to temporary folder..."
-cp "$filename" $mktemp_name || { rm -rf -- "$mktemp_name"; error 'Failed to copy file.'; }
+cp "$filename" $mktemp_name || clean_up 'Failed to copy file.' 
 
 echo "Build src file..."
 current_path=$(pwd)
 cd "$mktemp_name"
-go build -o "$executable_file_name" "$filename" || { rm -rf -- "$mktemp_name"; error "Failed compiling src file."; }
+go build -o "$executable_file_name" "$filename" || clean_up 'Failed compiling src file.'
 
 echo "Move executable file to current path..."
-cp "$executable_file_name" "$current_path" || { rm -rf -- "$mktemp_name"; error "Failed to move executable file"; }
+cp "$executable_file_name" "$current_path" || clean_up 'Failed to move executable file'
 
-rm -rf -- "$mktemp_name"
+clean_up
 succeful_message "Succeful."
